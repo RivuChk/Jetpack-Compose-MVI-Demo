95d26404aec53582af59cd8e4ab1791cf59b5081
Rivu <rivu.chakraborty6174@gmail.com>
2020-04-18 16:07:07 +0530

    test: add test cases for RemoteMovieDataStore

---
+++ app/src/test/java/dev/rivu/mvijetpackcomposedemo/moviesearch/data/remote/RemoteMovieDataStoreSpec.kt
@@ -0,0 +1,135 @@
+package dev.rivu.mvijetpackcomposedemo.moviesearch.data.remote
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import dev.rivu.mvijetpackcomposedemo.moviesearch.data.datafactory.dummyMovieDetail
+import dev.rivu.mvijetpackcomposedemo.moviesearch.data.datafactory.dummyMovieDetailResponse
+import dev.rivu.mvijetpackcomposedemo.moviesearch.data.datafactory.dummyMovieSearchResponse
+import dev.rivu.mvijetpackcomposedemo.moviesearch.data.model.Movie
+import io.reactivex.rxjava3.core.Single
+import org.mockito.ArgumentMatchers.anyString
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+
+class RemoteMovieDataStoreSpecTest : Spek({
+
+    val movieApi: MovieApi = mock()
+    val remoteDataStore by memoized { RemoteMovieDataStore(movieApi) }
+
+    describe("#${RemoteMovieDataStore::getMoviesStream.name}") {
+
+        it("should return Flowable.error with UnsupportedOperationException") {
+            val testObservable = remoteDataStore.getMoviesStream(anyString()).test()
+            testObservable.assertError(UnsupportedOperationException::class.java)
+        }
+    }
+
+    describe("#${RemoteMovieDataStore::getMovies.name}") {
+
+        context("movieApi returns error") {
+
+            val error = Exception("some error")
+
+            beforeEachGroup {
+                whenever(movieApi.searchMovies(query = anyString(), type = anyString()))
+                    .thenReturn(
+                        Single.error(error)
+                    )
+            }
+
+            it("should return the same error, wrapped in Single") {
+                val testObservable = remoteDataStore.getMovies("").test()
+                testObservable.assertError(error)
+            }
+        }
+
+        context("movieApi returns data") {
+
+            val dummyResponse = dummyMovieSearchResponse
+
+            val dummyMovieList = dummyResponse.movies.map {
+                Movie(
+                    imdbID = it.imdbID,
+                    poster = it.poster,
+                    title = it.title,
+                    type = it.type,
+                    year = it.year
+                )
+            }
+
+            beforeEachGroup {
+                whenever(
+                    movieApi.searchMovies(
+                        query = anyString(),
+                        type = anyString(),
+                        apikey = anyString()
+                    )
+                )
+                    .thenReturn(
+                        Single.just(dummyResponse)
+                    )
+            }
+
+            it("should return the same error, wrapped in Single") {
+                val testObservable = remoteDataStore.getMovies("").test()
+                testObservable.assertValue(dummyMovieList)
+            }
+        }
+    }
+
+    describe("#${RemoteMovieDataStore::addMovies.name}") {
+
+        it("should return Completable.error with UnsupportedOperationException") {
+            val testObservable = remoteDataStore.addMovies(listOf()).test()
+            testObservable.assertError(UnsupportedOperationException::class.java)
+        }
+    }
+
+    describe("#${RemoteMovieDataStore::getMovieDetail.name}") {
+
+        context("movieApi returns error") {
+
+            val error = Exception("some error")
+
+            beforeEachGroup {
+                whenever(movieApi.getMovieDetail(imdbId = anyString(), apikey = anyString()))
+                    .thenReturn(
+                        Single.error(error)
+                    )
+            }
+
+            it("should return the same error, wrapped in Single") {
+                val testObservable = remoteDataStore.getMovieDetail("").test()
+                testObservable.assertError(error)
+            }
+        }
+
+        context("movieApi returns data") {
+
+            val dummyResponse = dummyMovieDetailResponse
+
+            val dummyMovieDetail = dummyMovieDetail
+
+            beforeEachGroup {
+                whenever(movieApi.getMovieDetail(imdbId = anyString(), apikey = anyString()))
+                    .thenReturn(
+                        Single.just(dummyResponse)
+                    )
+            }
+
+            it("should return the same error, wrapped in Single") {
+                val testObservable = remoteDataStore.getMovieDetail("").test()
+                testObservable.assertValue(dummyMovieDetail)
+            }
+        }
+    }
+
+    describe("#${RemoteMovieDataStore::addMovieDetail.name}") {
+
+        it("should return Completable.error with UnsupportedOperationException") {
+            val testObservable = remoteDataStore.addMovies(listOf()).test()
+            testObservable.assertError(UnsupportedOperationException::class.java)
+        }
+    }
+})
