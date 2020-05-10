31fdaaed3dc39146aa1de8ab627849af7dc8e062
Rivu <rivu.chakraborty6174@gmail.com>
2020-05-10 19:53:42 +0530

    feat: write implementation logic for LocalDataStore

--- app/src/main/java/dev/rivu/mvijetpackcomposedemo/moviesearch/data/local/LocalMovieDataStore.kt
+++ app/src/main/java/dev/rivu/mvijetpackcomposedemo/moviesearch/data/local/LocalMovieDataStore.kt
@@ -3,59 +3,149 @@ package dev.rivu.mvijetpackcomposedemo.moviesearch.data.local
 import androidx.room.EmptyResultSetException
 import dev.rivu.mvijetpackcomposedemo.moviesearch.data.MovieDataStore
 import dev.rivu.mvijetpackcomposedemo.moviesearch.data.local.database.MovieDao
+import dev.rivu.mvijetpackcomposedemo.moviesearch.data.local.database.MovieEnitity
 import dev.rivu.mvijetpackcomposedemo.moviesearch.data.model.Movie
 import dev.rivu.mvijetpackcomposedemo.moviesearch.data.model.MovieDetail
+import hu.akarnokd.rxjava3.bridge.RxJavaBridge
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 
 class LocalMovieDataStore(private val movieDao: MovieDao) : MovieDataStore {
 
-    //Not Implementing DB (Room DB) here as Jetpack Compose has some conflicts with kapt
-    //once that's resolved, we will implement Room DB here
-
-    val movies: MutableSet<Movie> = mutableSetOf()
-    val movieDetails: MutableMap<String, MovieDetail> = mutableMapOf()
-
     override fun getMoviesStream(searchQuery: String): Flowable<List<Movie>> {
-        return Single.defer<List<Movie>> {
-            Single.just<List<Movie>>(
-                movies.filter {
-                    it.title.contains(searchQuery, ignoreCase = true)
+        return RxJavaBridge.toV3Flowable(
+            movieDao.getMoviesStream(searchQuery)
+                .map { list ->
+                    list.map {
+                        Movie(
+                            imdbID = it.imdbID,
+                            poster = it.poster,
+                            title = it.title,
+                            type = it.type,
+                            year = it.year
+                        )
+                    }
                 }
-            )
-        }.toFlowable()
+        )
     }
 
     override fun getMovies(searchQuery: String): Single<List<Movie>> {
-        return Single.defer<List<Movie>> {
-            Single.just<List<Movie>>(
-                movies.filter {
-                    it.title.contains(searchQuery, ignoreCase = true)
+        return RxJavaBridge.toV3Single(
+            movieDao.getMovies(searchQuery)
+                .map { list ->
+                    list.map {
+                        Movie(
+                            imdbID = it.imdbID,
+                            poster = it.poster,
+                            title = it.title,
+                            type = it.type,
+                            year = it.year
+                        )
+                    }
                 }
-            )
-        }
+        )
     }
 
     override fun addMovies(movieList: List<Movie>): Completable {
-        return Completable.fromCallable {
-            movies.addAll(movieList)
-        }
+        return return RxJavaBridge.toV3Completable(
+            movieDao.addMovies(
+                movieList.map {
+                    MovieEnitity(
+                        imdbID = it.imdbID,
+                        poster = it.poster,
+                        title = it.title,
+                        type = it.type,
+                        year = it.year
+                    )
+                }
+            )
+        )
     }
 
     override fun getMovieDetail(imdbId: String): Single<MovieDetail> {
-        return Single.defer {
-            if (movieDetails.containsKey(imdbId)) {
-                Single.just(movieDetails.get(imdbId))
-            } else {
-                Single.error(EmptyResultSetException("Movie not found in DB"))
-            }
-        }
+        return RxJavaBridge.toV3Single(
+            movieDao.getMovie(imdbId)
+                .map {
+                    if (it.detail == null) {
+                        throw EmptyResultSetException("detail not present for this movie")
+                    } else {
+                        MovieDetail(
+                            imdbID = it.imdbID,
+                            poster = it.poster,
+                            title = it.title,
+                            type = it.type,
+                            year = it.year,
+
+                            response = it.detail.response,
+                            actors = it.detail.actors,
+                            awards = it.detail.awards,
+                            boxOffice = it.detail.boxOffice,
+                            country = it.detail.country,
+                            dVD = it.detail.dVD,
+                            director = it.detail.director,
+                            genre = it.detail.genre,
+                            imdbRating = it.detail.imdbRating,
+                            imdbVotes = it.detail.imdbVotes,
+                            language = it.detail.language,
+                            metascore = it.detail.metascore,
+                            plot = it.detail.plot,
+                            production = it.detail.production,
+                            rated = it.detail.rated,
+                            ratings = it.detail.ratings.map {
+                                MovieDetail.Rating(
+                                    source = it.source,
+                                    value = it.value
+                                )
+                            },
+                            released = it.detail.released,
+                            runtime = it.detail.runtime,
+                            website = it.detail.website,
+                            writer = it.detail.writer
+                        )
+                    }
+                }
+        )
     }
 
     override fun addMovieDetail(movie: MovieDetail): Completable {
-        return Completable.fromAction {
-            movieDetails[movie.imdbID] = movie
-        }
+        return RxJavaBridge.toV3Completable(
+            movieDao.updateMovieInDB(
+                MovieEnitity(
+                    imdbID = movie.imdbID,
+                    poster = movie.poster,
+                    title = movie.title,
+                    type = movie.type,
+                    year = movie.year,
+                    detail = MovieEnitity.Detail(
+                        response = movie.response,
+                        actors = movie.actors,
+                        awards = movie.awards,
+                        boxOffice = movie.boxOffice,
+                        country = movie.country,
+                        dVD = movie.dVD,
+                        director = movie.director,
+                        genre = movie.genre,
+                        imdbRating = movie.imdbRating,
+                        imdbVotes = movie.imdbVotes,
+                        language = movie.language,
+                        metascore = movie.metascore,
+                        plot = movie.plot,
+                        production = movie.production,
+                        rated = movie.rated,
+                        ratings = movie.ratings.map {
+                            MovieEnitity.Detail.Rating(
+                                source = it.source,
+                                value = it.value
+                            )
+                        },
+                        released = movie.released,
+                        runtime = movie.runtime,
+                        website = movie.website,
+                        writer = movie.writer
+                    )
+                )
+            )
+        )
     }
 }
